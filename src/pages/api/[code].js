04d8ae0b138a0fe94006e0a97dd62f7eb08fb24c
randyglasgow <glasgowrandy@gmail.com>
2021-12-22 22:10:54 -0500

    Removes code

--- src/pages/api/[code].js
+++ src/pages/api/[code].js
@@ -1,38 +1,7 @@
 export default async function handler(req, res) {
-  // the code requested by the user
-  const requestCode = req.query.code;
-  const url = `https://${req.headers.host}/api/success/${requestCode}`;
-  console.log(url);
-  if (requestCode < 200) {
-    // not implemented yet
-    // return not implemented yet http response
-    res.status(501).json({
-      message: "not implemented yet",
-    });
-  }
-  if (requestCode < 300) {
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      const code = await response.status;
-      res.status(code).json(data);
-    } catch {
-      res.status(500).json({
-        message: "error",
-      });
-    }
-  }
-  if (requestCode < 400) {
-    // not implemented yet
-
-    res.status(501).json({
-      message: "not implemented yet",
-    });
-  }
-  if (requestCode < 500) {
-    // not implemented yet
-    res.status(501).json({
-      message: "not implemented yet",
-    });
-  }
+  // respond not implemented
+  console.log(req);
+  res.status(501).json({
+    message: "Not Implemented",
+  });
 }
