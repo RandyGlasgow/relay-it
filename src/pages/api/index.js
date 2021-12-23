8994e77ad98d3630a00daf7ea4720d1e591dac2d
randyglasgow <glasgowrandy@gmail.com>
2021-12-23 12:36:18 -0500

    Refators code /api/ root to show link to each response

--- src/pages/api/index.js
+++ src/pages/api/index.js
@@ -5,9 +5,27 @@ import { success_codes } from "./success";
 export default async function handler(req, res) {
   // all possible status codes grouped by category
   const statusCodes = {
-    success_codes: success_codes,
-    server_error_codes: server_error_codes,
-    client_error_codes: client_error_codes,
+    success_codes: success_codes.map(({ code, name }) => {
+      return {
+        name,
+        code,
+        url: `https://${req.headers.host}/api/success/${code}`,
+      };
+    }),
+    server_error_codes: server_error_codes.map(({ code, name }) => {
+      return {
+        name,
+        code,
+        url: `https://${req.headers.host}/api/server/${code}`,
+      };
+    }),
+    client_error_codes: client_error_codes.map(({ code, name }) => {
+      return {
+        name,
+        code,
+        url: `https://${req.headers.host}/api/client/${code}`,
+      };
+    }),
   };
 
   res.status(200).json(statusCodes);
