768f2117c4442918a76a13ce978716193737a028
randyglasgow <glasgowrandy@gmail.com>
2021-12-22 21:58:08 -0500

    Updates api to use host name

--- src/pages/api/server/index.js
+++ src/pages/api/server/index.js
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
     return {
       name,
       code,
-      url: `${process.env.API_URL}/api/server/${code}`,
+      url: `${req.headers.host}/api/server/${code}`,
     };
   });
   res.status(200).json(data);
--- src/pages/api/success/index.js
+++ src/pages/api/success/index.js
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
     return {
       name: code.name,
       code: code.code,
-      url: `${process.env.API_URL}/api/success/${code.code}`,
+      url: `${req.headers.host}/api/success/${code.code}`,
     };
   });
   res.status(200).json(data);
