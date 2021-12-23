4c00f44c4cabb4ce9b2d18db5f70b3a5a3926625
randyglasgow <glasgowrandy@gmail.com>
2021-12-22 21:49:13 -0500

    updates configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
+  // set the jit to true to enable the JIT compiler
+  // this is recommended for production
+  jit: false,
   content: [],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    // tailwind typography
+    require('@tailwindcss/typography'),
+  ],
 }
