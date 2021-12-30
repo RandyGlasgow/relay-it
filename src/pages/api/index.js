5a7ebdf1311a40fc5e387291306392e4ca3851ca
randyglasgow <glasgowrandy@gmail.com>
2021-12-30 04:53:41 -0500

    Refactor info into information

--- src/pages/api/index.js
+++ src/pages/api/index.js
@@ -1,15 +1,23 @@
 import { client_error_codes } from "./client";
+import { information_codes } from "./information";
 import { server_error_codes } from "./server";
 import { success_codes } from "./success";
 
 export default async function handler(req, res) {
   // all possible status codes grouped by category
   const statusCodes = {
-    success_codes: success_codes.map(({ code, name }) => {
+    client_error_codes: client_error_codes.map(({ code, name }) => {
       return {
         name,
         code,
-        url: `https://${req.headers.host}/api/success/${code}`,
+        url: `https://${req.headers.host}/api/client/${code}`,
+      };
+    }),
+    information_codes: information_codes.map(({ code, name }) => {
+      return {
+        name,
+        code,
+        url: `https://${req.headers.host}/api/information/${code}`,
       };
     }),
     server_error_codes: server_error_codes.map(({ code, name }) => {
@@ -19,11 +27,11 @@ export default async function handler(req, res) {
         url: `https://${req.headers.host}/api/server/${code}`,
       };
     }),
-    client_error_codes: client_error_codes.map(({ code, name }) => {
+    success_codes: success_codes.map(({ code, name }) => {
       return {
         name,
         code,
-        url: `https://${req.headers.host}/api/client/${code}`,
+        url: `https://${req.headers.host}/api/success/${code}`,
       };
     }),
   };
