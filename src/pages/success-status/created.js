84b3099b4b77e534b7fba62c4fe59f14a3d19b29
randyglasgow <glasgowrandy@gmail.com>
2021-12-31 14:00:23 -0500

    Adds layout component for global navigation

---
+++ src/pages/success-status/created.js
@@ -0,0 +1,9 @@
+import Layout from "@/layouts/Layout";
+
+export default function Created() {
+  return (
+    <div>
+      <h1>Created</h1>
+    </div>
+  );
+}
---
+++ src/pages/success-status/ok.js
@@ -0,0 +1,9 @@
+import Layout from "@/layouts/Layout";
+
+export default function OK() {
+  return (
+    <div>
+      <h1>OK</h1>
+    </div>
+  );
+}
