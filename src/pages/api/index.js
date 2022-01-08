94c82fca40d93d4c22d419985800d0e0af299588
randyglasgow <glasgowrandy@gmail.com>
2022-01-08 14:51:50 -0500

    Add generic greating for api

--- src/pages/api/index.js
+++ src/pages/api/index.js
@@ -4,37 +4,5 @@ import { server_error_codes } from "./server";
 import { success_codes } from "./success";
 
 export default async function handler(req, res) {
-  // all possible status codes grouped by category
-  const statusCodes = {
-    client_error_codes: client_error_codes.map(({ code, name }) => {
-      return {
-        name,
-        code,
-        url: `https://${req.headers.host}/api/client/${code}`,
-      };
-    }),
-    information_codes: information_codes.map(({ code, name }) => {
-      return {
-        name,
-        code,
-        url: `https://${req.headers.host}/api/information/${code}`,
-      };
-    }),
-    server_error_codes: server_error_codes.map(({ code, name }) => {
-      return {
-        name,
-        code,
-        url: `https://${req.headers.host}/api/server/${code}`,
-      };
-    }),
-    success_codes: success_codes.map(({ code, name }) => {
-      return {
-        name,
-        code,
-        url: `https://${req.headers.host}/api/success/${code}`,
-      };
-    }),
-  };
-
-  res.status(200).json(statusCodes);
+  res.send({ message: "Welcome to the relay-it API!" });
 }
