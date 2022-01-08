1cf0d3ef157a5ef5b20670bdde33cd91d6fc407f
randyglasgow <glasgowrandy@gmail.com>
2022-01-08 14:48:07 -0500

    Updates the title of the home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,6 @@
+import Head from "next/head";
+
+import { FaBroadcastTower, FaGithubAlt } from "react-icons/fa";
 import styles from "@/styles/Home.module.css";
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/home/Hero";
@@ -5,9 +8,12 @@ import Hero from "@/components/home/Hero";
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Relay It</title>
+      </Head>
       <Navbar />
       <Hero />
-      <div className='h-screen'></div>
+      <div className={styles.content}>test</div>
     </>
   );
 }
