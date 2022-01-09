bb1e12d1c8450f483c977000972e09cb927d6ae2
randyglasgow <glasgowrandy@gmail.com>
2022-01-08 20:04:29 -0500

    Remove await and async

--- src/pages/api/delay/[delay]/[...code].js
+++ src/pages/api/delay/[delay]/[...code].js
@@ -2,12 +2,12 @@ import handler from "@/pages/api/[...code]";
 /**
  * @description handles the case where a user would like a delay to be added to the response.
  */
-export default async function Delay(req, res) {
+export default function Delay(req, res) {
   // grab the time delay from the request
   const delay = parseInt(req.query.delay);
 
   // await the delay and respond to the request
-  await setTimeout((fn) => {
+  setTimeout((fn) => {
     handler(req, res);
   }, delay);
 }
