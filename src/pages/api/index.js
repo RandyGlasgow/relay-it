41b2040feadfd11932f8fa197c25357098aeaa70
randyglasgow <glasgowrandy@gmail.com>
2022-01-10 13:20:45 -0500

    update index js api

--- src/pages/api/index.js
+++ src/pages/api/index.js
@@ -1,7 +1,3 @@
-import { client_error_codes } from "./client";
-import { information_codes } from "./information";
-import { server_error_codes } from "./server";
-import { success_codes } from "./success";
 
 export default async function handler(req, res) {
   res.send({ message: "Welcome to the relay-it API!" });
