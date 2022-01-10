718e797da6d11e0307fd6dc64860a1f7111f095b
randyglasgow <glasgowrandy@gmail.com>
2022-01-10 13:06:50 -0500

    Remove unused code

--- src/pages/success-status/created.js
+++
@@ -1,9 +0,0 @@
-import Layout from "@/layouts/Layout";
-
-export default function Created() {
-  return (
-    <div>
-      <h1>Created</h1>
-    </div>
-  );
-}
--- src/pages/success-status/ok.js
+++
@@ -1,9 +0,0 @@
-import Layout from "@/layouts/Layout";
-
-export default function OK() {
-  return (
-    <div>
-      <h1>OK</h1>
-    </div>
-  );
-}
