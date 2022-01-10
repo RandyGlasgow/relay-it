e1e808a0996ed488701d87cb315d5e070d4b06bb
randyglasgow <glasgowrandy@gmail.com>
2022-01-10 13:08:50 -0500

    Add support for small media screens

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,11 @@
 import styles from "@/styles/Navbar.module.css";
 import Link from "next/link";
 import { FaBroadcastTower, FaGithubAlt } from "react-icons/fa";
+import { IoMenu } from "react-icons/io5";
+import { useState } from "react";
+
+import { Menu } from "@headlessui/react";
+
 import { useRouter } from "next/router";
 
 // menu options
@@ -52,6 +57,37 @@ const ExamplesButton = ({ active }) => {
   );
 };
 
+const SmallDeviceMenu = () => {
+  const [active, setActive] = useState(false);
+  return (
+    <div className={styles.smallDevice}>
+      <HomeButton />
+      <div>
+        <Menu>
+          <Menu.Button>
+            <div>
+              <button
+                className={active ? styles.menuButtonActive : styles.menuButton}
+                onClick={() => setActive((prev) => !prev)}
+              >
+                <IoMenu />
+              </button>
+            </div>
+          </Menu.Button>
+
+          <Menu.Items>
+            <div className={styles.menu}>
+              <Menu.Item>
+                <GithubButton />
+              </Menu.Item>
+            </div>
+          </Menu.Items>
+        </Menu>
+      </div>
+    </div>
+  );
+};
+
 export default function Navbar() {
   const router = useRouter();
 
@@ -59,14 +95,23 @@ export default function Navbar() {
     return href === router.pathname;
   };
 
+  const LargeDeviceMenu = () => {
+    return (
+      <div className={styles.largeDevice}>
+        <HomeButton />
+        <ul>
+          <DocsButton active={isActiveRoute("/docs")} />
+          <ExamplesButton active={isActiveRoute("/examples")} />
+          <GithubButton />
+        </ul>
+      </div>
+    );
+  };
+
   return (
-    <nav className={styles.nav}>
-      <HomeButton />
-      <ul>
-        <DocsButton active={isActiveRoute("/docs")} />
-        <ExamplesButton active={isActiveRoute("/examples")} />
-        <GithubButton />
-      </ul>
+    <nav>
+      <LargeDeviceMenu />
+      <SmallDeviceMenu />
     </nav>
   );
 }
