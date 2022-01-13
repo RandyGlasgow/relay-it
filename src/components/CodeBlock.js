af663ed1d012468d31f0f2247e262de032281fce
randyglasgow <glasgowrandy@gmail.com>
2022-01-13 04:45:39 -0500

    Add copy to codeblock

---
+++ src/components/CodeBlock.js
@@ -0,0 +1,12 @@
+import { useEffect } from "react";
+import Highlight from "react-highlight";
+import styles from "@/styles/CodeBlock.module.css";
+import "node_modules/highlight.js/styles/a11y-dark.css";
+
+export default function CodeBlock({ code, language }) {
+  return (
+    <div className={styles.codeBlock}>
+      <Highlight className={language}>{code}</Highlight>
+    </div>
+  );
+}
